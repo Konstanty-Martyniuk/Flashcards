7555ef5466517a8c01dd98b77356f23117a8c2db
Konstanty Martyniuk <constantin.radecky@gmail.com>
2021-08-28 20:45:45 +0200

    Delete Topics directory

--- Topics/The Set interface/Creating a set/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) {
-        Set<String> set = new TreeSet<>(Arrays.asList("Gamma", "Alpha", "Omega"));
-        System.out.println(set);
-    }
-}
